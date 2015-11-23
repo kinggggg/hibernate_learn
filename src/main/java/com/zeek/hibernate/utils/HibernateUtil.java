65b952bddce2a1604c6ebf19affce75415b661b1
zeek <liweibooooo@126.com>
2015-11-23 23:36:59 +0800

    简单介绍了下无状态session，即StatelessSession；若需要详细内容可以上网搜索

--- src/main/java/com/zeek/hibernate/utils/HibernateUtil.java
+++ src/main/java/com/zeek/hibernate/utils/HibernateUtil.java
@@ -32,6 +32,17 @@ public class HibernateUtil {
 		return sf.openSession();
 	}
 	
+	/**
+	 * 开启无状态session：
+	 * 在通常的情况下，hibernate中存在一级缓存即session级别的缓存和二级缓存即sessionFactory级别的缓存。session的一些操作比如说save，update
+	 * 等操作其实是和session一级缓存在做交互，并且在某些情况下session一级缓存中会缓存很多个对象，这样的话会占用大量的内存；在之前的笔记中还提到过
+	 * 在session一级缓存中还维护各个对象的一个镜像对象；有时候session一级缓存还要和二级缓存进行交互等等这些内容；有时候这些功能是不需要的，为了这些不必要的缓存工作此时就可以使用无状态的session
+	 * @return
+	 */
+	public static StatelessSession openStatelessSession(){
+		return sf.openStatelessSession();
+	}
+	
 	//回滚事务
 	public static void rollBackTx(Transaction tx){
 		if(tx != null && !tx.wasRolledBack()){
